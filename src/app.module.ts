0e7ea9d8a39ad8a97c91e4be594f18a61aa79570
Jubiko31 <jubiko.macaberidze1@gmail.com>
2022-11-15 16:30:18 +0400

    task module configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { TasksModule } from './tasks/tasks.module';
 @Module({
-  imports: [ConfigModule.forRoot({}), DatabaseModule],
+  imports: [ConfigModule.forRoot({}), DatabaseModule, TasksModule],
 })
 export class AppModule {}
