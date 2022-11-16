fe44473e0854f00d4371b525134a83d90bf8e9ea
Jubiko31 <jubiko.macaberidze1@gmail.com>
2022-11-16 12:11:27 +0400

    sign up with advanced credentials validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-creditionals.dto';
 import { UserRepository } from './repository/user.repository';
 
 @Injectable()
@@ -8,4 +9,12 @@ export class AuthService {
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
   ) {}
+
+  signUp(authCredentialsDto: AuthCredentialsDto): string {
+    const res = this.userRepository.signUp(authCredentialsDto);
+
+    return res
+      ? 'User account created successfully!'
+      : 'Error while creating new account';
+  }
 }
